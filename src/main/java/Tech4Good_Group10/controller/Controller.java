a0b98a6add99a344e9aa90b028ac60933d99fcfe
marinosrt <marina.rterol@gmail.com>
2023-06-20 15:56:18 +0200

    repository layer

--- src/main/java/Tech4Good_Group10/controller/Controller.java
+++ src/main/java/Tech4Good_Group10/controller/Controller.java
@@ -1,8 +1,18 @@
 package Tech4Good_Group10.controller;
 
+import Tech4Good_Group10.service.Service;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Tag(name = "Model Controller")
 public class Controller {
+
+    private final Service service;
+
 
 }
--- src/main/java/Tech4Good_Group10/model/Business.java
+++ src/main/java/Tech4Good_Group10/model/Business.java
@@ -1,14 +1,16 @@
 package Tech4Good_Group10.model;
 
+import lombok.Data;
+
+@Data
 public class Business {
 
-    private Long id;
-
     private TypeBusiness typeBusiness;
+
+    private String businessName;
 
     private String longitude;
 
     private String latitude;
-
 
 }

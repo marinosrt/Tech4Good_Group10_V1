6faeb4e4103812caa1f9bb6ce5105d3f0a61913c
Jaranag <118216573+Jaranag@users.noreply.github.com>
2023-06-20 15:46:07 +0200

    service

--- src/main/java/Tech4Good_Group10/model/Business.java
+++ src/main/java/Tech4Good_Group10/model/Business.java
@@ -4,11 +4,63 @@ public class Business {
 
     private Long id;
 
+    private String name;
+
     private TypeBusiness typeBusiness;
 
     private String longitude;
 
     private String latitude;
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public TypeBusiness getTypeBusiness() {
+        return typeBusiness;
+    }
+
+    public void setTypeBusiness(TypeBusiness typeBusiness) {
+        this.typeBusiness = typeBusiness;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    public Business(Long id, String name, TypeBusiness typeBusiness, String longitude, String latitude) {
+        this.id = id;
+        this.name = name;
+        this.typeBusiness = typeBusiness;
+        this.longitude = longitude;
+        this.latitude = latitude;
+    }
+
+    public Business() {
+
+    }
 }

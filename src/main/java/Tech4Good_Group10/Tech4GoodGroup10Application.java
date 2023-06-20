9579fd0c2d17ec74458313ef55030e8b0f5330f7
marinosrt <marina.rterol@gmail.com>
2023-06-20 12:21:12 +0200

    Initial commit of the project

---
+++ src/main/java/Tech4Good_Group10/Tech4GoodGroup10Application.java
@@ -0,0 +1,13 @@
+package Tech4Good_Group10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tech4GoodGroup10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tech4GoodGroup10Application.class, args);
+	}
+
+}
---
+++ src/test/java/Tech4Good_Group10/Tech4GoodGroup10ApplicationTests.java
@@ -0,0 +1,13 @@
+package Tech4Good_Group10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tech4GoodGroup10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
